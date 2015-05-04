685c25eab4678edcf6a49f1f5fba0a5f1314d043
Ram Subramanian <needmoarram@gmail.com>
2015-05-04 13:26:58 -0700

    Moved to Raphael js

--- lockscreen.js
+++ lockscreen.js
@@ -1,51 +1,63 @@
-var canvas = document.getElementById('a');
-var ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+var ww = rw = window.innerWidth;
+var wh = rh = window.innerHeight;
 
-var sw = window.innerWidth / 10;
-var cw = 2 * sw;
-var sh = sw / 2;
+if (rw * 1.775 > rh)
+    rw = rh / 1.775;
+else
+    rh = rw * 1.775;
 
-ctx.beginPath();
-ctx.arc(sw + cw / 2, 200, cw / 2, 0, 2 * Math.PI);
-ctx.stroke();
+var paper = Raphael((ww - rw) / 2, 0, rw, rh);
 
-ctx.beginPath();
-ctx.arc(sw * 2 + cw * 3 / 2, 200, cw / 2, 0, 2 * Math.PI);
-ctx.stroke();
+var brad = rw  *  15 / 128;
+var blef = rw  *  11 / 128;
+var bhsp = rw  *   8 / 128;
+var btop = rh * 163 / 568;
+var bvsp = rh *  13 / 568;
 
-ctx.beginPath();
-ctx.arc(sw * 3 + cw * 5 / 2, 200, cw / 2, 0, 2 * Math.PI);
-ctx.stroke();
+var buttons = []
+var nattr = {
+	fill: "#fff",
+	stroke: "#000",
+}
+var hlattr = {
+	fill: "#000",
+	stroke: "#000"
+}
 
-ctx.beginPath();
-ctx.arc(sw + cw / 2, 200 + sh + cw, cw / 2, 0, 2 * Math.PI);
-ctx.stroke();
+for (var i = 0; i < 3; i ++) {
+    for (var j = 0; j < 3; j ++) {
+        buttons[3 * i + j] = paper.circle(blef + bhsp * j + brad * (2 * j + 1),
+                                          btop + bvsp * i + brad * (2 * i + 1),
+                                          brad);
+        var p = paper.text(blef + bhsp * j + brad * (2 * j + 1),
+				           btop + bvsp * i + brad * (2 * i + 1) - rh / 100,
+				           3 * i + j + 1);
+        p.attr({font: "200 " + rh * 25 / 476 + "px Helvetica neue, sans-serif"});
+    }
+}
 
-ctx.beginPath();
-ctx.arc(sw * 2 + cw * 3 / 2, 200 + sh + cw, cw / 2, 0, 2 * Math.PI);
-ctx.stroke();
+buttons[3 * i] = paper.circle(blef + bvsp + brad * 3,
+                              btop + bvsp * 3 + brad * 7,
+                              brad);
 
-ctx.beginPath();
-ctx.arc(sw * 3 + cw * 5 / 2, 200 + sh + cw, cw / 2, 0, 2 * Math.PI);
-ctx.stroke();
+for (var b in buttons) {
+	buttons[b].attr(nattr);
+	(function (b, n) {
+		b[0].onmousedown = function() { b.animate(hlattr, 200); };
+		b[0].onmouseup = function() { b.animate(nattr, 200); };
+		b[0].onmouseleave = function() { b.animate(nattr, 200); };
+	})(buttons[b], b);
+}
 
-ctx.beginPath();
-ctx.arc(sw + cw / 2, 200 + 2 * (sh + cw), cw / 2, 0, 2 * Math.PI);
-ctx.stroke();
+// var p = paper.text(blef + brad, btop + brad, "8");
+// p.attr({font: "200 " + rh * 25 / 476 + "px Helvetica neue, sans-serif"});
 
-ctx.beginPath();
-ctx.arc(sw * 2 + cw * 3 / 2, 200 + 2 * (sh + cw), cw / 2, 0, 2 * Math.PI);
-ctx.stroke();
+/* var trad = rw *  5 / 256;
+var tlef = rw * 99 / 320;
+var thsp = rw * 24 / 320;
+var ttop = rh * 103 / 568;
 
-ctx.beginPath();
-ctx.arc(sw * 3 + cw * 5 / 2, 200 + 2 * (sh + cw), cw / 2, 0, 2 * Math.PI);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.arc(sw * 2 + cw * 3 / 2, 200 + 3 * (sh + cw), cw / 2, 0, 2 * Math.PI);
-ctx.stroke();
-
-console.log(canvas);
-
+for (var i = 0; i < 4; i ++)
+    drawCircle(tlef + thsp * i + trad * (2 * i + 1),
+               ttop + trad,
+               trad); */
